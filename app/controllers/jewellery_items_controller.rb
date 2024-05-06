83ca328447a33cef5ba186edde61cfb630edf5bf
nicola camp <nicola.camp89@yahoo.com>
2024-05-06 14:49:25 +0100

    show and create method completed

--- app/controllers/jewellery_items_controller.rb
+++ app/controllers/jewellery_items_controller.rb
@@ -4,4 +4,30 @@ class JewelleryItemsController < ApplicationController
   def index
     @jewellery_items = JewelleryItem.all
   end
+
+  def show
+    @jewellery_item = JewelleryItem.find(params[:id])
+  end
+
+  def new
+    @jewellery_item = JewelleryItem.new
+  end
+  
+  def create
+    @jewellery_item = JewelleryItem.new(jewellery_item_params)
+    @jewellery_item.user = User.first  # Just for testing, replace with actual user from session later
+    if @jewellery_item.save
+      redirect_to jewellery_item_path(@jewellery_item)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+
+  private
+
+  def jewellery_item_params
+    params.require(:jewellery_item).permit(:name, :description, :price, :jewellery_type, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'jewellery_items#index'
   resources :jewellery_items
 end
+
+# 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
